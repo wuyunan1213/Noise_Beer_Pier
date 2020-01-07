19e416bac0e892654b99b1a6bea40e159328f89a
Charles Wu <charleswu.01@gmail.com>
2020-01-07 15:06:39 -0500

    changes from glm to lm

--- CueWeightsforLily.R
+++ CueWeightsforLily.R
@@ -35,7 +35,7 @@ CalculateCueWeights <- function(responseV, predictorV1, predictorV2, method){
   #predictorV1 is the x1
   #predictorV2 is x2
   #Note that the output is a long format with the two cue weights stacked in rows for each participant so make sure to reshape the output matrix
-  mod <- glm(responseV ~ predictorV1 + predictorV2, family = 'binomial')
+  mod <- lm(responseV ~ predictorV1 + predictorV2)
   weight1 <- round(as.numeric(mod$coefficients[2]), digits = 3)
   weight2 <- round(as.numeric(mod$coefficients[3]), digits = 3)
   if (method == 'raw') {
@@ -60,7 +60,8 @@ CueWeights <- NULL
 
 for (i in 1:length(filenames)) {
     raw <- read.csv(filenames[i]) ###read in the raw data
-    Raw_weights <- ddply(raw, ~SubjectNumber+ConditionNumber, summarise, VOT = CalculateCueWeights(Response, VOTlevel, F0level, method = method)[1], F0 = CalculateCueWeights(Response, VOTlevel, F0level, method = method)[2])
+    trial_ave <- ddply(raw, ~SubjectNumber+ConditionNumber+VOTlevel+F0level, summarise, perPIER = sum(Response == 1)/length(Response))
+    Raw_weights <- ddply(trial_ave, ~SubjectNumber+ConditionNumber, summarise, VOT = CalculateCueWeights(perPIER, VOTlevel, F0level, method = method)[1], F0 = CalculateCueWeights(perPIER, VOTlevel, F0level, method = method)[2])
     ###since we are missing subject number in the file, we need to paste this into the data
     weights <- melt(Raw_weights, id.vars = c('SubjectNumber','ConditionNumber'), variable.name = 'Dimensions', value.name = 'CW')
     weights$SubjectNumber <- extract_numeric(filenames[i])
@@ -76,13 +77,13 @@ for (i in 1:length(filenames)) {
 ###########step 4###################
 ###Next we can try to draw bar plots to show the cue weights for different conitions
 ##We first get an average cue weighs across all subjects
-aveCW <- ddply(CueWeights, ~ConditionNumber+Dimensions+AgeGroup, summarise, meanCW = mean(CW), n = length(CW), sd = sd(CW), se = sd/sqrt(n))
+aveCW <- ddply(CueWeights, ~ConditionNumber+Dimensions, summarise, meanCW = mean(CW), n = length(CW), sd = sd(CW), se = sd/sqrt(n))
 aveCW$ConditionNumber <- as.factor(aveCW$ConditionNumber)
 ggplot(aveCW, aes(ConditionNumber, meanCW, group = Dimensions, fill = Dimensions))+
   geom_bar(stat = 'identity', position = position_dodge())+
   scale_x_discrete(labels=c('65/-3', '65/0', '65/3', '75/-3', '75/0', '75/3', '65/quiet','75/quiet'))+
   geom_errorbar(aes(ymin = meanCW-se, ymax = meanCW+se), position = position_dodge())+
-  ggtitle('Cue Weights in different conditions(raw, using glm)')+
+  ggtitle('Cue Weights in different conditions(raw, using lm)')+
   theme(axis.text.x = element_text(size = 10, face = 'bold', angle = 40))
   
 ########step5#######
@@ -105,7 +106,7 @@ ggplot(ave[ave$AgeGroup == 'younger',], aes(ConditionNumber, mean, fill = Dimens
   geom_bar(stat = 'identity', position = position_dodge())+
   geom_errorbar(aes(ymin=mean-se, ymax = mean+se), width = 0.8, position = position_dodge(0.9), size = 0.9)+
   scale_x_discrete(labels = c('-3SNR/65dB', '0SNR/65dB', '+3SNR/65dB', 'quiet/65dB'))+
-  scale_y_continuous(limits = c(-0.5,4))+
+  scale_y_continuous(limits = c(-0.5,0.5))+
   labs(size = 15,
        x = 'Signal to noise levels',
        y = 'Cue Weights',
@@ -113,6 +114,21 @@ ggplot(ave[ave$AgeGroup == 'younger',], aes(ConditionNumber, mean, fill = Dimens
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5), axis.title = element_text(face = 'bold', size = 18), axis.text.x = element_text(face = 'bold', color = 'black', size = 15), axis.text.y = element_text(face = 'bold', color = 'black', size = 15), legend.text = element_text(face = 'bold', color = 'black', size = 14), legend.title = element_text(face = 'bold', color = 'black', size = 14), title = element_text(face = 'bold', size = 20))
 dev.off()
+
+png("CueWeights_older.png", units = "in", width=10, height=8, res=500)
+ggplot(ave[ave$AgeGroup == 'older',], aes(ConditionNumber, mean, fill = Dimensions))+
+  geom_bar(stat = 'identity', position = position_dodge())+
+  geom_errorbar(aes(ymin=mean-se, ymax = mean+se), width = 0.8, position = position_dodge(0.9), size = 0.9)+
+  scale_x_discrete(labels = c('-3SNR/65dB', '0SNR/65dB', '+3SNR/65dB', 'quiet/65dB'))+
+  scale_y_continuous(limits = c(-0.5,0.5))+
+  labs(size = 15,
+       x = 'Signal to noise levels',
+       y = 'Cue Weights',
+       title = 'Cue Weights in older group')+
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5), axis.title = element_text(face = 'bold', size = 18), axis.text.x = element_text(face = 'bold', color = 'black', size = 15), axis.text.y = element_text(face = 'bold', color = 'black', size = 15), legend.text = element_text(face = 'bold', color = 'black', size = 14), legend.title = element_text(face = 'bold', color = 'black', size = 14), title = element_text(face = 'bold', size = 20))
+dev.off()
+
 
 Ano_Dimensions <- aov(CW ~ (Dimensions*ConditionNumber) + Error(SubjectNumber/(Dimensions*ConditionNumber)), data=newCueWeights)
 summary(Ano_Dimensions)
